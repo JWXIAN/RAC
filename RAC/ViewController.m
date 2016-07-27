58d2bde405a028b2e70f8a90d5343d560255652b
GJW <320891162@qq.com>
2016-07-27 17:57:57 +0800

    简单的接受信号

--- RAC/ViewController.m
+++ RAC/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *btn;
 
 @property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UITextField *textPwd;
 @end
 
 @implementation ViewController
@@ -25,16 +26,40 @@
         return [RACSignal empty];
     }];
     
-    [[self.textField.rac_textSignal
-      filter:^BOOL(id value){
-          NSString*text = value;
-          return text.length > 3;
-      }]
-     subscribeNext:^(id x){
-         NSLog(@"%@", x);
+//    [[self.textField.rac_textSignal
+//      filter:^BOOL(NSString *value){
+//          NSString *text = value;
+//          return text.length > 3;
+//      }]
+//     subscribeNext:^(id x){
+//         NSLog(@"%@", x);
+//     }];
+    
+//    //信号 长度
+//   [[[self.textField.rac_textSignal map:^id(NSString *value) {
+//       return @(value.length);
+//    }]filter:^BOOL(NSNumber *value) {
+//        return [value integerValue] > 3;
+//    }]
+//    subscribeNext:^(id x) {
+//        NSLog(@"%@", x);
+//    }];
+    
+    RACSignal *userNameSignal = [self.textField.rac_textSignal map:^id(NSString *value) {
+        return @([self isValidUsername:value]);
+    }];
+    
+    //接受信号，改变背景色
+    RAC(self.textField, backgroundColor) =
+    [userNameSignal
+     map:^id(NSNumber *passwordValid){
+         return[passwordValid boolValue] ? [UIColor clearColor]:[UIColor redColor];
      }];
 }
 
+- (BOOL)isValidUsername:(NSString *)username {
+    return username.length > 3;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
